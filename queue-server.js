6f84216dbbf09f4000f2b85efac1e98e42f5006f
Simone Odegard <Simone.Odegard@gmail.com>
2021-05-06 21:13:41 -0700

    communicating with queue-server but not sending deliveries

---
+++ queue-server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const PORT = process.env.QUEUE_SERVER || 3001;
+const uuid = require('uuid').v4;
+const io = require('socket.io')(PORT);
+
+const queue = {
+  messages: {}
+};
+
+const vendor = io.of('/vendor');
+
+vendor.on('connection', socket => {
+
+  socket.on('deliveredMessage', payload => {
+    let id = uuid();
+    queue.messages[id] = payload;
+    console.log('received message', queue);
+
+    // socket.emit('added');
+    vendor.emit('delivered', { id, payload });
+
+  });
+
+  socket.on('getAll', () => {
+
+    console.log('yyayayyayayayaaaaaaaaa');
+
+    Object.keys(queue.messages).forEach(id => {
+      vendor.emit('message', { id, payload: queue.messages[id] });
+    });
+
+  });
+
+  socket.on('received', message => {
+
+    delete queue.messages[message.id];
+    console.log('deleted message');
+
+  });
+
+});
